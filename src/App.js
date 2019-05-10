59ed703310412e589d641b85cc8f07b52c39718d
baixingxing <email@e920702811@qq.com>
2019-05-10 12:19:40 +0800

    111

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from 'antd-mobile';
+import {connect} from 'react-redux';
+import {counter,addGun,removeGun,addGunAsync} from './index.redux'
+import 'antd-mobile/dist/antd-mobile.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+@connect(
+  state=>({num:state}),
+  {addGun,removeGun,addGunAsync}
+)
+class App extends React.Component{
+  render(){
+    return (
+      <div>
+        <h1>现在有武器{this.props.num}</h1>
+        <Button onClick={this.props.addGun}>加武器</Button>
+        <Button onClick={this.props.removeGun}>减武器</Button>
+        <Button onClick={this.props.addGunAsync}>拖两天给</Button>
+      </div>
+    )
+  }
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {createStore,applyMiddleware,compose} from 'redux'
+import {counter} from './index.redux'
+import {Provider} from 'react-redux'
+import 'antd-mobile/dist/antd-mobile.css';
+import thunk from 'redux-thunk'
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+const store=createStore(counter, compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension?window.devToolsExtension():f=>f
+))
+ReactDOM.render((
+    <Provider store={store}><App /></Provider>), document.getElementById('root'));
+// import {createStore} from 'redux'
+// function counter(state=0,action){
+//     switch(action.type){
+//         case '加机关枪':
+//             return state+1
+//         case '减机关枪':
+//             return state-1
+//         default:
+//             return state=10
+//     }
+// }
+// const store=createStore(counter)
+// function listener(){
+//     const current=store.getState();
+//     console.log(`现在有机关枪${current}`)
+// }
+// store.subscribe(listener)
+// console.log(store.getState())
+// store.dispatch({type:"加机关枪"})
+// console.log(store.getState())
+// store.dispatch({type:"加机关枪"})
+// console.log(store.getState())
+// store.dispatch({type:"减机关枪"})
+// console.log(store.getState())
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
