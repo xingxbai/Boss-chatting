97907eb8ef013c8aeb8a219378ca0ecfb41fc558
baixingxing <email@e920702811@qq.com>
2019-05-20 11:13:41 +0800

    打包编译build

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-const express = require('express')
+import express from 'express'
 const app = express()
+const path=require('path')
 const server=require('http').createServer(app)
 const io=require('socket.io')(server)
 const bodyParser = require('body-parser')
@@ -12,7 +13,6 @@ io.on('connection',function(socket){
 	socket.on('sendMsg',function(data){
 		const {from,to,content}=data
 		const chatId=[from,to].sort().join('_')
-		console.log(data)
 		Chat.create({from,to,content,chatId},function(err,doc){
 			if(!err){
 				io.emit('recvmsg',Object.assign({},doc._doc))
@@ -21,11 +21,17 @@ io.on('connection',function(socket){
 	})
 })
 app.use(cookieParser())
+
+
+app.use(function(req,res,next){
+	if(req.url.startsWith('/user/')||req.url.startsWith('/static/')){
+		return next()
+	}else{
+		return res.sendFile(path.resolve('build/index.html'))
+	}
+})
+app.use(express.static(path.resolve('build')))
 app.use(bodyParser.json())
 app.use('/user',userRouter)
 server.listen(9093,function(){
 })
-
-
-
-;
